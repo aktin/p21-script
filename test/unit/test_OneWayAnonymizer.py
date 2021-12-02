58f9825d78cfddd95add15adb9f86fb4e1c49e3c
Alexander Kombeiz <alexkombie@gmail.com>
2021-12-02 12:24:47 +0100

    adjusted unittest for anonymizer to test java alg name conversion

--- test/unit/test_OneWayAnonymizer.py
+++ test/unit/test_OneWayAnonymizer.py
@@ -1,21 +1,22 @@
 import unittest
+
 from src.p21import import OneWayAnonymizer
 
 
 class TestOneWayAnonymizer(unittest.TestCase):
 
     def test_anonymize_sha1(self):
-        anonymizer = OneWayAnonymizer()
+        anonymizer = OneWayAnonymizer('')
         hash1 = anonymizer.anonymize('root', 'ext', 'salt')
         self.assertEqual('2dIpFJQG_BflkTgkYYbHdnY59hM=', hash1)
 
     def test_anonymize_sha1_missing_salt(self):
-        anonymizer = OneWayAnonymizer()
+        anonymizer = OneWayAnonymizer('SHA-1')
         hash1 = anonymizer.anonymize('root', 'ext', '')
         self.assertEqual('3OLW8Md-0-jVketgW0ovUVEji6g=', hash1)
 
     def test_anonymize_sha1_list(self):
-        anonymizer = OneWayAnonymizer()
+        anonymizer = OneWayAnonymizer('sha1')
         list_ext = ['ext1', 'ext2', 'ext3']
         list_hash = anonymizer.anonymize_list('root', list_ext, 'salt')
         self.assertEqual('65AfbJVhzZIUDIsAPgeJ1AXtpAs=', list_hash[0])
