21dfaba9453412e6e5610e664beeb9661885cbf8
Alexander Kombeiz <alexkombie@gmail.com>
2021-12-08 17:27:12 +0100

    added test to check and rename 'fab' to 'fachabteilung'

--- test/unit/test_CSVPreprocessor.py
+++ test/unit/test_CSVPreprocessor.py
@@ -1,10 +1,12 @@
-import unittest
 import os
-import pandas as pd
+import unittest
+
 import chardet
+import pandas as pd
+
+from src.p21import import CSVPreprocessor, FABPreprocessor, FALLPreprocessor, ICDPreprocessor, OPSPreprocessor
 from src.p21import import TmpFolderManager
 from src.p21import import ZipFileExtractor
-from src.p21import import CSVPreprocessor, FALLPreprocessor, FABPreprocessor, ICDPreprocessor, OPSPreprocessor
 
 
 def get_csv_encoding(path_csv: str) -> str:
@@ -20,7 +22,7 @@ def read_csv_header(preprocessor: CSVPreprocessor) -> str:
 
 class TestCSVPreprocessor(unittest.TestCase):
 
-    def setUp(self) -> None:
+    def setUp(self):
         path_parent = os.path.dirname(os.getcwd())
         path_resources = os.path.join(path_parent, 'resources')
         path_zip = os.path.join(path_resources, 'p21_preprocess.zip')
@@ -29,7 +31,7 @@ class TestCSVPreprocessor(unittest.TestCase):
         self.PATH_TMP = self.TMP.create_tmp_folder()
         zfe.extract_zip_to_folder(self.PATH_TMP)
 
-    def tearDown(self) -> None:
+    def tearDown(self):
         self.TMP.remove_tmp_folder()
 
     def test_preprocess_FALL(self):
@@ -55,6 +57,18 @@ class TestCSVPreprocessor(unittest.TestCase):
                             'fabentlassungsdatum', 'kennungintensivbett'}
         columns_matched = columns_required.intersection(columns_csv)
         self.assertTrue(columns_matched == columns_required)
+
+    def test_preprocess_FAB_alt(self):
+        FABPreprocessor.CSV_NAME = 'fab_alt.csv'
+        fab = FABPreprocessor(self.PATH_TMP)
+        fab.preprocess()
+        header = read_csv_header(fab)
+        columns_csv = set(header.split(';'))
+        columns_required = {'ik', 'entlassenderstandort', 'entgeltbereich', 'khinterneskennzeichen', 'standortnummerbehandlungsort', 'fachabteilung', 'fabaufnahmedatum',
+                            'fabentlassungsdatum', 'kennungintensivbett'}
+        columns_matched = columns_required.intersection(columns_csv)
+        self.assertTrue(columns_matched == columns_required)
+        FABPreprocessor.CSV_NAME = 'fab.csv'
 
     def test_preprocess_ICD(self):
         icd = ICDPreprocessor(self.PATH_TMP)
