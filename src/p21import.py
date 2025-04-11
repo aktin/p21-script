e2b0eb71370f90dfc8e2957f789460b9fd7eaf77
Alexander Ivanets <sunberry00@proton.me>
2025-04-11 07:47:56 +0200

    Revert "Remove verify_file in main"
    This reverts commit 36804209e507c92257bcb2a750849fdd001ab281.

--- src/p21import.py
+++ src/p21import.py
@@ -1348,7 +1348,9 @@ if __name__ == '__main__':
     if len(sys.argv) != 3:
         raise SystemExit('sys.argv don\'t match')
     p21 = P21Importer(sys.argv[2])
-    if sys.argv[1] == 'import_file':
+    if sys.argv[1] == 'verify_file':
+        p21.verify_file()
+    elif sys.argv[1] == 'import_file':
         p21.import_file()
     else:
         raise SystemExit('unknown method function')
